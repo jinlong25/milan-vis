003babeb0ffe14a4eabe8f3049cbf07c0deb4c38
Jinlong Yang <jinlong25@users.noreply.github.com>
2022-05-21 22:16:01 -0700

    set up xG slope chart

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
-//create a namespace for canvas
-var cv = {
+//create a namespace for shotMap
+var sm = {
   'width': 930,
-  'height':780,
+  'height': 780,
 	'top': 50,
 	'right': 10,
 	'bottom': 10,
 	'left': 10,
   'fieldLineWidth': 2
 };
+
+var xs = {
+  'width': 930,
+  'height': 780,
+  'top': 10,
+  'right': 50,
+  'bottom': 10,
+  'left': 50,
+}
 
 var milan_players = [
   '7193',
@@ -41,138 +50,46 @@ var milan_players = [
 ];
 
 //create scales for shot X/Y
-var xScaleUnderstat = d3.scaleLinear()
+sm.xScaleUnderstat = d3.scaleLinear()
   .domain([0, 1])
-  .range([cv.height, 0]);
+  .range([sm.height, 0]);
 
-var yScaleUnderstat = d3.scaleLinear()
+sm.yScaleUnderstat = d3.scaleLinear()
   .domain([0, 1])
-  .range([cv.width, 0]);
+  .range([sm.width, 0]);
 
-//create a svg as main cavnas
-var svg = d3.select('#canvas').append('svg')
-  .attr('width', cv.width + cv.left + cv.right)
-  .attr('height', cv.height + cv.top + cv.bottom + 1000);
+//create a svg for shot map
+var shotMap = d3.select('#shot_map').append('svg')
+  .attr('width', sm.width + sm.left + sm.right)
+  .attr('height', sm.height + sm.top + sm.bottom + 300);//##
+
+//create a svg for xG slope
+var xG_slope = d3.select('#xG_slope').append('svg')
+.attr('width', xs.width + xs.left + xs.right)
+.attr('height', xs.height + xs.top + xs.bottom);
 
 //draw the soccer field in svg
 //draw field background
-svg.append('rect')
+shotMap.append('rect')
   .attr('x', 0)
   .attr('y', 0)
-  .attr('width', cv.width + cv.top + cv.right)
-  .attr('height', cv.height + cv.top + cv.bottom + 1000)
+  .attr('width', sm.width + sm.top + sm.right)
+  .attr('height', sm.height + sm.top + sm.bottom + 1000)
   .attr('fill', '#222222')
   .attr('fill-opacity', 0.9);
 
 drawFieldLines();
 
 //read in the data from csv
-d3.csv('data/data.csv').then(
+d3.csv('data/data-all.csv').then(
   function(data) {
+    //get data for xG slope
+    var xGSlopeData = data.filter(isAMilanShot);
 
-    //filter goals
-    // var selectedShots = data.filter(isAMilanGoal);
-    // var selectedShots = data;
-    var selectedPlayerId = '7193'
-    var selectedShots = data.filter(function(obj) {
-      return obj.player_id == selectedPlayerId || obj.player_id == '11111';
-    });
-
-    var selectedShotsCoord = Object.keys(selectedShots).map((key) => [xScaleUnderstat(parseFloat(selectedShots[key].Y)), yScaleUnderstat(parseFloat(selectedShots[key].X))]);
-    console.log(selectedShotsCoord);
-
-    var color = d3.scaleSequential(d3.interpolateLab("white", "steelblue"))
-        .domain([0, 3]);
-
-    var hexbin = d3.hexbin()
-    // .size([1000, 30])
-        .radius(30)
-        .extent([[0, 0], [cv.width, cv.height]]);
-        // .extent([[cv.left, cv.top], [(cv.height + cv.top), (cv.width + cv.left)]]);
-        // .extent([[10, 50], [50, 60]]);
-        // .extent([[cv.top, cv.left], [cv.height + cv.top, cv.width + cv.left]]);
-
-    svg.append("clipPath")
-        .attr("id", "clip")
-        .append("rect")
-        .attr('transform', 'translate(' + cv.left + ', ' + cv.top + ')')
-        .attr("width", cv.width)
-        .attr("height", cv.height);
-
-    svg.append("g")
-        .attr("class", "hexagon")
-        .attr('transform', 'translate(' + cv.left + ', ' + cv.top + ')')
-        // .attr("clip-path", "url(#clip)")
-        .selectAll("path")
-        .data(hexbin(selectedShotsCoord))
-        .enter().append("path")
-        .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
-        .attr("d", hexbin.hexagon())
-        .attr("fill", function(d) {
-          return color(d.length);
-        });
-
-          console.log(hexbin.extent());
+    console.log(xGSlopeData);
 
 
-    //plot all goals
-    svg.append('g')
-      .attr('class', 'goals')
-      .attr('transform', 'translate(' + cv.left + ' ' + cv.top + ')')
-      .selectAll('.goal')
-      .data(selectedShots)
-      .enter().append('circle')
-      .attr('class', 'goal')
-      .attr('data-id', d => d.id)
-      .attr('data-minute', d => d.minute)
-      .attr('data-result', d => d.result)
-      .attr('data-X', d => d.X)
-      .attr('data-X-scaled', d => xScaleUnderstat(d.X))//##
-      .attr('data-Y', d => d.Y)
-      .attr('data-a_goals', d => d.a_goals)
-      .attr('data-a_team', d => d.a_team)
-      .attr('data-date', d => d.date)
-      .attr('data-h_a', d => d.h_a)
-      .attr('data-h_goals', d => d.h_goals)
-      .attr('data-h_team', d => d.h_team)
-      .attr('data-lastAction', d => d.lastAction)
-      .attr('data-match_id', d => d.match_id)
-      .attr('data-player', d => d.player)
-      .attr('data-player_assisted', d => d.player_assisted)
-      .attr('data-player_id', d => d.player_id)
-      .attr('data-season', d => d.season)
-      .attr('data-shotType', d => d.shotType)
-      .attr('data-situation', d => d.situation)
-      .attr('data-xG', d => d.xG)
-      .attr('r', 3)
-      .attr('cy', d => xScaleUnderstat(parseFloat(d.X)))
-      .attr('cx', d => yScaleUnderstat(parseFloat(d.Y)))
-      .attr('fill-opacity', 0)
-      // .attr('stroke', '#FFFF00')
-      .attr('stroke', function(d){
-        if(d.X == '0' && d.Y == '0') {
-          return 'white';
-        } else if (d.X == '1' && d.Y == '0') {
-          return 'green';
-        } else if (d.X == '0' && d.Y == '1') {
-          return 'yellow';
-        } else if (d.X == '1' && d.Y == '1') {
-          return 'red';
-        } else {
-          return 'yellow';
-        }
-      })
-      .attr('stroke-width', 1.2)
-      .attr('stroke-opacity', 1);
-
-    //add mouseover
-    d3.selectAll('.goal').on('mouseover', function(d){
-      var thisGoal = d3.select(this);
-      console.log(thisGoal.attr('data-player') + ' ' + thisGoal.attr('data-h_team') + ' vs ' + thisGoal.attr('data-a_team'));
-      console.log(thisGoal.attr('data-X') + ', ' + thisGoal.attr('data-Y'));
-      console.log(yScaleUnderstat(thisGoal.attr('data-X')) + ', ' + xScaleUnderstat(thisGoal.attr('data-Y')));
-    });
-
+    drawShotmap(data);
   }
 );
 
@@ -186,43 +103,153 @@ function isAMilanGoal(obj) {
   // return obj.result == 'Goal' || obj.result == 'OwnGoal'
 };
 
+function isAMilanShot(obj) {
+  return milan_players.includes(obj.player_id);
+};
+
+function drawShotmap(data) {
+
+  //filter goals
+  // var selectedShots = data.filter(isAMilanGoal);
+  // var selectedShots = data;
+  var selectedPlayerId = '7193'
+  var selectedShots = data.filter(function(obj) {
+    return obj.player_id == selectedPlayerId || obj.player_id == '11111';
+  });
+
+  var selectedShotsCoord = Object.keys(selectedShots).map((key) => [sm.xScaleUnderstat(parseFloat(selectedShots[key].Y)), sm.yScaleUnderstat(parseFloat(selectedShots[key].X))]);
+  console.log(selectedShotsCoord);
+
+  var color = d3.scaleSequential(d3.interpolateLab("white", "steelblue"))
+      .domain([0, 3]);
+
+  var hexbin = d3.hexbin()
+  // .size([1000, 30])
+      .radius(30)
+      .extent([[0, 0], [sm.width, sm.height]]);
+      // .extent([[sm.left, sm.top], [(sm.height + sm.top), (sm.width + sm.left)]]);
+      // .extent([[10, 50], [50, 60]]);
+      // .extent([[sm.top, sm.left], [sm.height + sm.top, sm.width + sm.left]]);
+
+  shotMap.append("clipPath")
+      .attr("id", "clip")
+      .append("rect")
+      .attr('transform', 'translate(' + sm.left + ', ' + sm.top + ')')
+      .attr("width", sm.width)
+      .attr("height", sm.height);
+
+  shotMap.append("g")
+      .attr("class", "hexagon")
+      .attr('transform', 'translate(' + sm.left + ', ' + sm.top + ')')
+      // .attr("clip-path", "url(#clip)")
+      .selectAll("path")
+      .data(hexbin(selectedShotsCoord))
+      .enter().append("path")
+      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
+      .attr("d", hexbin.hexagon())
+      .attr("fill", function(d) {
+        return color(d.length);
+      });
+
+        console.log(hexbin.extent());
+
+
+  //plot all goals
+  shotMap.append('g')
+    .attr('class', 'goals')
+    .attr('transform', 'translate(' + sm.left + ' ' + sm.top + ')')
+    .selectAll('.goal')
+    .data(selectedShots)
+    .enter().append('circle')
+    .attr('class', 'goal')
+    .attr('data-id', d => d.id)
+    .attr('data-minute', d => d.minute)
+    .attr('data-result', d => d.result)
+    .attr('data-X', d => d.X)
+    .attr('data-X-scaled', d => sm.xScaleUnderstat(d.X))//##
+    .attr('data-Y', d => d.Y)
+    .attr('data-a_goals', d => d.a_goals)
+    .attr('data-a_team', d => d.a_team)
+    .attr('data-date', d => d.date)
+    .attr('data-h_a', d => d.h_a)
+    .attr('data-h_goals', d => d.h_goals)
+    .attr('data-h_team', d => d.h_team)
+    .attr('data-lastAction', d => d.lastAction)
+    .attr('data-match_id', d => d.match_id)
+    .attr('data-player', d => d.player)
+    .attr('data-player_assisted', d => d.player_assisted)
+    .attr('data-player_id', d => d.player_id)
+    .attr('data-season', d => d.season)
+    .attr('data-shotType', d => d.shotType)
+    .attr('data-situation', d => d.situation)
+    .attr('data-xG', d => d.xG)
+    .attr('r', 3)
+    .attr('cy', d => sm.xScaleUnderstat(parseFloat(d.X)))
+    .attr('cx', d => sm.yScaleUnderstat(parseFloat(d.Y)))
+    .attr('fill-opacity', 0)
+    // .attr('stroke', '#FFFF00')
+    .attr('stroke', function(d){
+      if(d.X == '0' && d.Y == '0') {
+        return 'white';
+      } else if (d.X == '1' && d.Y == '0') {
+        return 'green';
+      } else if (d.X == '0' && d.Y == '1') {
+        return 'yellow';
+      } else if (d.X == '1' && d.Y == '1') {
+        return 'red';
+      } else {
+        return 'yellow';
+      }
+    })
+    .attr('stroke-width', 1.2)
+    .attr('stroke-opacity', 1);
+
+  //add mouseover
+  d3.selectAll('.goal').on('mouseover', function(d){
+    var thisGoal = d3.select(this);
+    console.log(thisGoal.attr('data-player') + ' ' + thisGoal.attr('data-h_team') + ' vs ' + thisGoal.attr('data-a_team'));
+    console.log(thisGoal.attr('data-X') + ', ' + thisGoal.attr('data-Y'));
+    console.log(sm.yScaleUnderstat(thisGoal.attr('data-X')) + ', ' + sm.xScaleUnderstat(thisGoal.attr('data-Y')));
+  });
+}
+
 function drawFieldLines() {
-  var field = svg.append('g')
+  var field = shotMap.append('g')
     .attr('class', 'field-lines')
-    .attr('transform', 'translate(' + cv.left + ' ' + cv.top + ')' );
+    .attr('transform', 'translate(' + sm.left + ' ' + sm.top + ')' );
 
   //draw side lines
   field.append('line')
     .attr('x1', 0)
     .attr('x2', 0)
     .attr('y1', 0)
-    .attr('y2', cv.height)
-    .attr('stroke-width', cv.fieldLineWidth)
+    .attr('y2', sm.height)
+    .attr('stroke-width', sm.fieldLineWidth)
     .attr('stroke', '#ffffff');
 
   field.append('line')
-    .attr('x1', cv.width)
-    .attr('x2', cv.width)
+    .attr('x1', sm.width)
+    .attr('x2', sm.width)
     .attr('y1', 0)
-    .attr('y2', cv.height)
-    .attr('stroke-width', cv.fieldLineWidth)
+    .attr('y2', sm.height)
+    .attr('stroke-width', sm.fieldLineWidth)
     .attr('stroke', '#ffffff');
 
   //draw end lines
   field.append('line')
     .attr('x1', 0)
-    .attr('x2', cv.width)
+    .attr('x2', sm.width)
     .attr('y1', 0)
     .attr('y2', 0)
-    .attr('stroke-width', cv.fieldLineWidth)
+    .attr('stroke-width', sm.fieldLineWidth)
     .attr('stroke', '#ffffff');
 
   field.append('line')
     .attr('x1', 0)
-    .attr('x2', cv.width)
-    .attr('y1', cv.height)
-    .attr('y2', cv.height)
-    .attr('stroke-width', cv.fieldLineWidth)
+    .attr('x2', sm.width)
+    .attr('y1', sm.height)
+    .attr('y2', sm.height)
+    .attr('stroke-width', sm.fieldLineWidth)
     .attr('stroke', '#ffffff');
 
   //draw midfield circle
@@ -234,7 +261,7 @@ function drawFieldLines() {
     .attr('x2', 245)
     .attr('y1', 0)
     .attr('y2', 180)
-    .attr('stroke-width', cv.fieldLineWidth)
+    .attr('stroke-width', sm.fieldLineWidth)
     .attr('stroke', '#ffffff');
 
   field.append('line')
@@ -242,7 +269,7 @@ function drawFieldLines() {
     .attr('x2', 685)
     .attr('y1', 0)
     .attr('y2', 180)
-    .attr('stroke-width', cv.fieldLineWidth)
+    .attr('stroke-width', sm.fieldLineWidth)
     .attr('stroke', '#ffffff');
 
   field.append('line')
@@ -250,7 +277,7 @@ function drawFieldLines() {
     .attr('x2', 685)
     .attr('y1', 180)
     .attr('y2', 180)
-    .attr('stroke-width', cv.fieldLineWidth)
+    .attr('stroke-width', sm.fieldLineWidth)
     .attr('stroke', '#ffffff');
 
   //draw goal box
@@ -259,7 +286,7 @@ function drawFieldLines() {
     .attr('x2', 365)
     .attr('y1', 0)
     .attr('y2', 60)
-    .attr('stroke-width', cv.fieldLineWidth)
+    .attr('stroke-width', sm.fieldLineWidth)
     .attr('stroke', '#ffffff');
 
   field.append('line')
@@ -267,7 +294,7 @@ function drawFieldLines() {
     .attr('x2', 565)
     .attr('y1', 0)
     .attr('y2', 60)
-    .attr('stroke-width', cv.fieldLineWidth)
+    .attr('stroke-width', sm.fieldLineWidth)
     .attr('stroke', '#ffffff');
 
   field.append('line')
@@ -275,7 +302,7 @@ function drawFieldLines() {
     .attr('x2', 565)
     .attr('y1', 60)
     .attr('y2', 60)
-    .attr('stroke-width', cv.fieldLineWidth)
+    .attr('stroke-width', sm.fieldLineWidth)
     .attr('stroke', '#ffffff');
 };
 
